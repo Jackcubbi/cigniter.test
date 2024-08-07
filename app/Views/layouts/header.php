e0be87cd4057e3db4eff255237d85755ce97835a
Jackcubbi <jackcubbi@gmail.com>
2024-08-07 23:41:40 +0300

    edit: menu in header.php

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -7,10 +7,10 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Главная</a>
+          <a class="nav-link" href="/cigniter.test">Главная</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">О нас</a>
+          <a class="nav-link" href="/cigniter.test/tasks">Задания</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Услуги</a>
