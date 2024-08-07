c401ab9a47c22e795767daa088b036c3ea0c0cc8
Jackcubbi <jackcubbi@gmail.com>
2024-08-08 00:14:40 +0300

    edit: migration tables to database

---
+++ app/Database/Migrations/2024-08-07-205641_CreateTask.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTask extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'              => 'INT',
+                'constraint'        => 5,
+                'unsigned'          => true,
+                'auto_increment'    => true,
+
+            ],
+
+            'title' => [
+                'type'              => 'VARCHAR',
+                'constraint'        => 100,
+            ],
+
+            'description' => [
+                'type'              => 'TEXT',
+            ],
+
+            'created_at' => [
+                'type'              => 'DATETIME',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tasks');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tasks');
+    }
+}
